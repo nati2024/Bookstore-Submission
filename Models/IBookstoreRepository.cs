8f5cd97ef6f86c459817eccf2dafaf88ff9f2c6c
nati2024 <95377889+nati2024@users.noreply.github.com>
2023-03-01 16:23:57 -0700

    Add project files.

---
+++ Models/IBookstoreRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    // an interface is not a class... but rather a template for a class that will ensure things are used correctly
+    public interface IBookstoreRepository
+    {
+        // IQueryable takes the place of the lists that we've used
+        // it's a class set up specifically for querying data
+        IQueryable<Books> Books { get; }
+    }
+}
