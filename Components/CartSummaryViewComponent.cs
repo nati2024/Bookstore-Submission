ca3ed2433ad13b89998dc1a1fad7a01f32bb4b98
nati2024 <95377889+nati2024@users.noreply.github.com>
2023-03-11 16:42:49 -0700

    Some changes we made to mission 11

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Bookstore.Models;
+
+namespace Bookstore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket basketService)
+        {
+            basket = basketService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
